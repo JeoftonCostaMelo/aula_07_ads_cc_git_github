ef90137319c1ae6983336a10b016845b6a0aea3a
JeoftonCosta <jeofton_costa@hotmail.com>
2025-05-02 10:22:34 -0300

    Implementados os laços while e for

---
+++ Aula_07/laco_for.py
@@ -0,0 +1,10 @@
+# for elemento in range(0, 101, 20):
+#     print(elemento)
+
+nomes = ['João', "Paulo", 'Pedro', 'João', 'Gabriela', 'Jonas', 'Ana Julia']
+
+for n in nomes:
+    if n == 'Gabriela':
+        continue
+    print(n)
+print('Fim do programa')
--- Aula_07/laco_while.py
+++ Aula_07/laco_while.py
@@ -1,10 +1,19 @@
 import datetime
 
-escolha = ''
-while escolha != 'n':
-    ano_de_nascimento = int(input('Digite o ano de nascimento do usuário e tecle enter: '))
-    ano_atual = datetime.date.today().year
-    idade = ano_atual - ano_de_nascimento
-    print(f"A idade do usuario é {idade}")
-    escolha = input('Deseja fazer uma nova verificação? S ou N').lower()
-print('\nFim do programa')
+# escolha = ''
+# while escolha != 'n':
+#     ano_de_nascimento = int(input('Digite o ano de nascimento do usuário e tecle enter: '))
+#     ano_atual = datetime.date.today().year
+#     idade = ano_atual - ano_de_nascimento
+#     print(f"A idade do usuario é {idade}")
+#     escolha = input('Deseja fazer uma nova verificação? S ou N').lower()
+# print('\nFim do programa')
+
+
+contadora = 0
+while contadora <= 50:
+    contadora += 1
+    if contadora % 3 == 0:
+        continue
+    print(contadora)
+print('\nFim do programa.')
