dd4bb90f98fd5f880c807155f58b972de21ed988
JeoftonCosta <jeofton_costa@hotmail.com>
2025-05-02 09:15:31 -0300

    Implementada a verificação da idade.

---
+++ Aula_07/laco_while.py
@@ -0,0 +1,10 @@
+import datetime
+
+escolha = ''
+while escolha != 'n':
+    ano_de_nascimento = int(input('Digite o ano de nascimento do usuário e tecle enter: '))
+    ano_atual = datetime.date.today().year
+    idade = ano_atual - ano_de_nascimento
+    print(f"A idade do usuario é {idade}")
+    escolha = input('Deseja fazer uma nova verificação? S ou N').lower()
+print('\nFim do programa')
